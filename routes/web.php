944bfe7d3bc80121b23530ee5096dd41270ae6c0
UniquelySimilar <consciousexplorer@gmail.com>
2017-05-03 16:54:55 -0600

    began customer functionality

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,17 @@
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::get('/', 'CustomerController@index');
 
+// Register the typical authentication routes for an application
+// Illuminate/Support/Facades/Auth.php
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::resource('customers', 'CustomerController');
